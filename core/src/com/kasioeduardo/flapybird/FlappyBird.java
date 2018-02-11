12de673158a9fc273f8b4788381446d9651b5d19
Kásio Eduardo <kasioeduardo13@gmail.com>
2018-02-11 11:17:56 -0200

    adicionado queda no pássaro.

--- core/src/com/kasioeduardo/flapybird/FlappyBird.java
+++ core/src/com/kasioeduardo/flapybird/FlappyBird.java
@@ -8,10 +8,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch; //CLASSE PARA CRIAR AS ANIMAÇÕES
-    private Texture[] passaro;
+    private Texture[] passaros;
     private Texture fundo;
     private int larguraDispositivo,alturaDispositivo;
+
+
     private float variacao = 0;
+    private float velocidadeQueda =0;
+    private float posicaoInicialVertical = 0;
 	
 	@Override
 	public void create () {
@@ -20,21 +24,27 @@ public class FlappyBird extends ApplicationAdapter {
         fundo   = new Texture("fundo.png");
 
         // imagens de animação do passaro.
-	    passaro = new Texture[3];
-	    passaro[0] = new Texture("passaro1.png");
-	    passaro[1] = new Texture("passaro2.png");
-	    passaro[2] = new Texture("passaro2.png");
+	    passaros = new Texture[3];
+	    passaros[0] = new Texture("passaro1.png");
+	    passaros[1] = new Texture("passaro2.png");
+	    passaros[2] = new Texture("passaro2.png");
 
 
 	    larguraDispositivo = Gdx.graphics.getWidth();
 	    alturaDispositivo  = Gdx.graphics.getHeight();
+        posicaoInicialVertical = alturaDispositivo /2;
 	}
 
 	@Override
 	public void render () {
 	    //CHAMADO DE TEMPO EM TEMPO PARA CRIAR ANIMAÇÕES DENTRO DO JOGO
 
-        variacao++;
+        variacao += Gdx.graphics.getDeltaTime() * 8; //getDeltaTime -> VARIAÇÃO ENTRE AS CHAMADAS DO RENDER
+        velocidadeQueda++;
+
+        if (posicaoInicialVertical > 0 ) posicaoInicialVertical -= velocidadeQueda; // o passaro não cai d[a tea~sd,LÇ
+
+
         if (variacao > 2) variacao = 0; // resetando o VETOR
 
         batch.begin(); //iniciando exibição das imgs
@@ -43,7 +53,7 @@ public class FlappyBird extends ApplicationAdapter {
                 a ordem que desenhamos as imagens é importante, pois define quem vem 'por cima' de quem.
              */
         batch.draw(fundo,0,0, larguraDispositivo,alturaDispositivo); // ocupando o tamanho da tela com a imagem, idependente do tamanho do dispositivo.
-        batch.draw(passaro,200,alturaDispositivo/2);//configuração de onde o pássaro vai aparecer na tela, eixo X,Y
+        batch.draw(passaros[(int)variacao],200,posicaoInicialVertical);//configuração de onde o pássaro vai aparecer na tela, eixo X,Y
 
 
         batch.end();// finalizando a exibicao das imgs
