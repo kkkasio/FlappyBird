c364aa8cec138ec047f2e137b0fea1ec3ccf00fd
Kásio Eduardo <kasioeduardo13@gmail.com>
2018-02-11 12:13:14 -0200

    feito a variação nos canos verticalmente de forma aleatória.

--- core/src/com/kasioeduardo/flapybird/FlappyBird.java
+++ core/src/com/kasioeduardo/flapybird/FlappyBird.java
@@ -6,22 +6,36 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch; //CLASSE PARA CRIAR AS ANIMAÇÕES
     private Texture[] passaros;
     private Texture fundo;
+    private Texture canoBaixo;
+    private Texture canoTopo;
     private int larguraDispositivo,alturaDispositivo;
+
+    private Random numeroRandomico;
 
 
     private float variacao = 0;
     private float velocidadeQueda =0;
     private float posicaoInicialVertical = 0;
+    private float espacoEntreCanos;
+    private float posicaoMovimentoCanoHorizontal;
+    private float deltaTime;
+    private float alturaEntreCanosRandomica;
 	
 	@Override
 	public void create () {
 
 	    batch   = new SpriteBatch();
         fundo   = new Texture("fundo.png");
+
+        //textura dos canos
+        canoBaixo = new Texture("cano_baixo.png");
+        canoTopo  = new Texture("cano_topo.png");
 
         // imagens de animação do passaro.
 	    passaros = new Texture[3];
@@ -33,19 +47,39 @@ public class FlappyBird extends ApplicationAdapter {
 	    larguraDispositivo = Gdx.graphics.getWidth();
 	    alturaDispositivo  = Gdx.graphics.getHeight();
         posicaoInicialVertical = alturaDispositivo /2;
+
+        numeroRandomico = new Random();
+
+        posicaoMovimentoCanoHorizontal = larguraDispositivo -100;
+        espacoEntreCanos = 300;
 	}
 
 	@Override
 	public void render () {
+	    deltaTime = Gdx.graphics.getDeltaTime(); //getDeltaTime -> VARIAÇÃO ENTRE AS CHAMADAS DO RENDER
 	    //CHAMADO DE TEMPO EM TEMPO PARA CRIAR ANIMAÇÕES DENTRO DO JOGO
 
-        variacao += Gdx.graphics.getDeltaTime() * 8; //getDeltaTime -> VARIAÇÃO ENTRE AS CHAMADAS DO RENDER
-        velocidadeQueda++;
+        variacao += deltaTime * 8;
+        if (variacao > 2) variacao = 0; // resetando o VETOR da imagem do pássaro.
 
-        if (posicaoInicialVertical > 0 ) posicaoInicialVertical -= velocidadeQueda; // o passaro não cai d[a tea~sd,LÇ
+        velocidadeQueda++;//velocidade de queda deve aumentar para que o passaro tenha o efeito de queda.
+        if(Gdx.input.justTouched()){
+            // quando a tela for tocada o pássaro deve SALTAR! (SUBIR)
+            velocidadeQueda = -20; // como estamos aumentando a veloc de queda, quando a tela for pressionada esta velocidade tem que ser subtraida, assim o passaro irá saltar.
+        }
+        if (posicaoInicialVertical > 0 ||velocidadeQueda < 0) posicaoInicialVertical -= velocidadeQueda; // quando passaro cai ele fica no fim e náo passa para baixo da tela.
+
+        posicaoMovimentoCanoHorizontal -= deltaTime * 200; // fazendo os canos andar para a esquerda na tela
+
+        //verifica se o cano saiu inteiramente da tela para aparecer outro cano.
+        if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()+2) {
+            posicaoMovimentoCanoHorizontal = larguraDispositivo;//volta o cano para o inicio da tela na direita.
+            // cada vez que o cano sai da tela ele deve ter uma NOVA ALTURA;
+            alturaEntreCanosRandomica = numeroRandomico.nextInt(400)-200; // gerando a altura POSITIVA e NEGATIVA
+            Gdx.app.log("Espaço Randomico: ", "" +alturaEntreCanosRandomica);
+        }
 
 
-        if (variacao > 2) variacao = 0; // resetando o VETOR
 
         batch.begin(); //iniciando exibição das imgs
             /*
@@ -53,6 +87,8 @@ public class FlappyBird extends ApplicationAdapter {
                 a ordem que desenhamos as imagens é importante, pois define quem vem 'por cima' de quem.
              */
         batch.draw(fundo,0,0, larguraDispositivo,alturaDispositivo); // ocupando o tamanho da tela com a imagem, idependente do tamanho do dispositivo.
+        batch.draw(canoTopo, posicaoMovimentoCanoHorizontal,alturaDispositivo /2 + espacoEntreCanos /2 + alturaEntreCanosRandomica); //
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo /2 - canoBaixo.getHeight() - espacoEntreCanos /2 + alturaEntreCanosRandomica); //
         batch.draw(passaros[(int)variacao],200,posicaoInicialVertical);//configuração de onde o pássaro vai aparecer na tela, eixo X,Y
 
 
